c30e077c72203ad62504ae12c4a01b1f7c52da9c
Alice <sousaku_0912@yahoo.co.jp>
2021-07-03 17:56:24 +0900

    first

---
+++ basic/main.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+const score = 75;
+
+// if (score > 80) {
+//     console.log('great !!');
+// } else if (score > 60) {
+//     console.log('normal');
+// } else {
+//     console.log('low...');
+// }
+
+score >= 80 ? console.log('great') : console.log('normal');
