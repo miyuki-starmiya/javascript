408d8f0483b8ad49f483b7c02adb2cde5445e317
KamiHitoe <sousaku_0912@yahoo.co.jp>
2021-07-08 05:42:05 +0900

    0708

--- basic/demo.py
+++ basic/demo.py
@@ -1,6 +1,30 @@
 
 import random
+import math
+from datetime import datetime
+import time
 
-print(random.randint(3,5))
+d = datetime.now()
 
+print(d.strftime('%Y%m%d'))
+
+class C:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def static():
+        print('static')
+
+    @classmethod
+    def cl(cls):
+        print('class')
+
+    def ins(self):
+        print('ins')
+
+C.static()
+C.cl()
+c = C('hoge')
+c.ins()
 
--- basic/main.js
+++ basic/main.js
@@ -1,23 +1,11 @@
 
 'use strict';
 let scores = [60, 70, 80];
-let other = [10, 20, 30];
-const print = console.log;
+let i = 0;
 
-let sum = 0;
-scores.forEach(score => {
-    sum += score;
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('submit');
 })
 
-for (let i=0; i<scores.length; i++) {
-    sum += scores[i];
-}
-
-scores.forEach(
-    function (score) {
-        return sum += score;
-    }
-)
-
-print(sum);
 
