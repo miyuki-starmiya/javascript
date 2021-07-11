45ed03ad2b5d23c332cbf0407cd59f69a28d16d6
KamiHitoe <sousaku_0912@yahoo.co.jp>
2021-07-11 13:41:05 +0900

    0711

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,24 +1,32 @@
 
 (function() {
+    // validate type miss
     'use strict';
     let vm = new Vue({
+        // Controllerの設定
         el: '#app',
+        // Modelの変数群
         data: {
             newItem: '',
-            todos: [{
-                title: 'task 1',
-                isDone: false,
-            }, {
-                title: 'task 2',
-                isDone: false,
-            }, {
-                title: 'task 3',
-                isDone: true,
+            todos: [],
+        },
+        // save data in localStorage
+        watch: {
+            todos: {
+                handler: function() {
+                    localStorage.setItem('todos', JSON.stringify(this.todos));
+                    // alert('Data saved');
+                },
+                deep: true
             },
-            ],
+        },
+        // localStorageからdataを読み込む
+        mounted: function() {
+            this.todos = JSON.parse(localStorage.getItem('todos')) || [];
         },
         methods: {
             addItem: function() {
+                // this = data
                 let item = {
                     title: this.newItem,
                     isDone: false,
@@ -30,8 +38,24 @@
                 if (confirm('are you sure?')) {
                     this.todos.splice(i, 1);
                 }
-            }
-        }
+            },
+            purge: function() {
+                if (confirm('delete finished?')) {
+                    this.todos = this.todos.filter(function(todo) {
+                        return !todo.isDone;
+                    });
+                }
+            },
+        },
+        // 関数の戻り値を変数として利用
+        computed: {
+            remaining: function() {
+                let items = this.todos.filter(function(todo) {
+                    return !todo.isDone;
+                });
+                return items.length;
+            },
+        },
     });
 })();
 
