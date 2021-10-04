33a440bb070a62b85938db8a509dd31d63b95021
Alice <sousaku_0912@yahoo.co.jp>
2021-10-05 08:33:58 +0900

    test vue stripe

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,8 @@
 
-hoge = 1;
-let fuga = 2;
 
-console.log(hoge, fuga);
+var stripe = Stripe('pk_test_51JgcxjAQjSIe0mCFI2ToTYGFo0bljksOstzBEtWgd8ZWXO9KyBPgq0RoTDbDJGwGWrLUWL6evWcirlI3zg6kagBE00kBvDObQK');
+var elements = stripe.elements(); // cardのエレメントをつくる
+console.log(Stripe);
+console.log(elements);
+
 
