8da9957059452c82e34ea27bef1e53b55ab49fec
Alice <sousaku_0912@yahoo.co.jp>
2022-08-20 19:04:53 +0900

    m

--- nodejs/demo/app.js
+++ nodejs/demo/app.js
@@ -1,25 +1,10 @@
-var express      = require('express')
-var cookieParser = require('cookie-parser')
 
-var app = express()
-app.use(express.static('public'));
-
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
+  res.send('hello express')
+});
 
-app.get('/process_get', (req, res) => {
-  var response = {
-    first_name: req.query.first_name,
-    last_name: req.query.last_name,
-  };
-  console.log(response);
-  res.end(JSON.stringify(response));
-})
-
-var server = app.listen(8081, () => {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Example app listening at http://%s:%s", host, port);
-})
+console.log('start listening');
+app.listen(3000);
 
